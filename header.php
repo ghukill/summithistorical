d5e31d52c33ecc309400ae8e566565f71a3a730f
Graham Hukill <ghukill@gmail.com>
2015-06-27 21:41:05 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  <link href='http://fonts.googleapis.com/css?family=Old+Standard+TT:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css">    
+</head>
+
+<body>
+<div class="container">
+<header >
+   <div class="row">
+      <div class="col-xs-offset-4 col-xs-4"> 
+           <img src="img/logo.png" class="img-responsive logo">
+      </div>
+  </div>
+  <div class="row hidden-xs">
+     <div class="col-xs-offset-1 col-xs-1 nav "><a href="#">HOME</a></div>
+     <div class="col-xs-1 nav"><a href="#">ABOUT</a></div>
+     <div class="col-xs-1 nav"><a href="#">SITE</a></div>
+     <div class="col-xs-1 nav"><a href="#">EVENTS</a></div>
+     <div class="col-xs-1 nav"><a href="#">ARCHIVES</a></div>
+     <div class="col-xs-1 nav"><a href="#">TOURS</a></div>
+     <div class="col-xs-1 nav"><a href="#">JOIN</a></div>
+     <div class="col-xs-1 nav"><a href="#">BOOKSTORE</a></div>
+     <div class="col-xs-1 nav"><a href="#">CONTACT</a></div>
+  </div>
+</header>
