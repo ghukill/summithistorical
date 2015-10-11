32277252112eab0203047289ea2deb11962668d7
Graham Hukill <ghukill@gmail.com>
2015-10-11 16:26:40 -0400

    archives

--- archives.php
+++ archives.php
@@ -38,11 +38,169 @@
         <!-- load header -->
         <?php include("assets/templates/header.php");?>
 
-        <!-- ################ PAGE CONTENT ################  -->
-
-        
+        <div class="space-70"></div>
 
         <!-- ################ PAGE CONTENT ################  -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Archives and Research</h1>
+
+                    <!-- coming soon-->
+                    <div class="alert">
+                        <h3>Coming Soon!</h3>
+                        <p>Old Dillon Schoolhouse Archives, and the Mather Archives (Sandy Mathers) in Breckenridge.</p>
+                    </div>
+
+                    <!-- content -->
+                    <h3>Overview</h3>
+
+                    <p>The Summit Historical Society has a large number of archival materials and artifacts in its possession. Its holdings of historical Summit County photographs, maps, and government documents are extensive. Similarly, there is a growing collection of biographical data concerning Summit County figures. Once estimated at 17,000 separate items, the Society's artifacts collection is enormous and is still growing. Many items are either on display in the Society's properties or are on loan in the area for public view.</p>
+
+                    <h3>To Search the Archives</h3>
+
+                    <p>Summit County Library Search Directions for SHS Collections.  Most items in the Society’s archival collections have been catalogued for online access via the Summit County Library and Marmot systems.</p>
+
+                    </p>To SEARCH for a specific named item, use the following directions:</p>
+
+                    <ol>
+                        <li>Using your favorite search engine, search for Summit County Public Library Catalog Home or http://summit.opac.marmot.org</li>
+                        <li>When the catalog appears, select searching by either Keyword or Title.  Type in the name of the object of your search.</li>
+                        <li>Once the record appears, click on the title/heading for the record for details.</li>
+                        <li>Click on the “Notes” tab for a full description of the item.</li>
+                        <li>If you see the notation “Buy a Copy,” and you are interested in a photo or map, contact the archives at <a href="mailto:archives@summithistorical.org">archives@summithistorical.org</a>.</li>
+                    </ol>
+
+                    <p>To BROWSE the Society’s catalogued collections, use the following directions:</p>
+
+                    <ol>
+                        <li>Using your favorite search engine, search for Summit County Public Library Catalog Home or http://summit.opac.marmot.org</li>
+                        <li>Under Keyword search, type one of the following:
+                            <ul>
+                                <li>For photographs:  summit county (colo) historical society photographs</li>
+                                <li>When the listing appears, click Narrow Your Results to Photo.   For an alphabetical listing, sort by Title.</li>
+                            </ul>
+                        </li>
+
+                        <li>For maps: summit county (colo) historical society maps This is a work in progress.  Some maps will have a map  icon; others will have a book icon.  For an alphabetical listing, sort by Title and scroll to “Abe Lee placer.”</li>
+
+                        <li>For manuscripts, government records and books: summit county (colo) historical society manuscripts.  This is another work in progress.  When the listing appears, click Entire Collection.  For an alphabetical listing, sort by Title.</li>
+
+                        <li>For the Burke-Martin Collection:  summit county (colo) historical society burke-martin mine reports, click Books for records; click Photo for photograph; click Map for maps.</li>
+
+                        <li>For an overview of the Society’s entire catalogued archival holdings:  Dillon Schoolhouse Museum</li>
+                    </ol>
+
+                    <p>ADDITIONAL SEARCH TIPS for PHOTOGRAPHS:  Many of the catalogued photographs contain the names of their original subject categories and using those subject terms can aid in your search.  Popular subject search terms include the following:</p>
+
+                    <ul>
+                        <li>Argentine, Colorado</li>
+                        <li>Breckenridge, Colorado</li>
+                        <li>Chihuahua, Colorado</li>
+                        <li>Dillon, Colorado or New Dillon, Colorado or Old Dillon, Colorado</li>
+                        <li>Dredge mining</li>
+                        <li>Green Mountain, Colorado</li>
+                        <li>Kokomo, Colorado</li>
+                        <li>Lode mining</li>
+                        <li>Lumbering</li>
+                        <li>Masons</li>
+                        <li>Montezuma, Colorado</li>
+                        <li>Odd Fellows</li>
+                        <li>Placer mining</li>
+                        <li>Portraits (Use the last name of the person you are seeking)</li>
+                        <li>Railroads</li>
+                        <li>Robinson, Colorado</li>
+                        <li>Sts. John, Colorado</li>
+                        <li>Skiing</li>
+                        <li>Slate Creek, Colorado</li>
+                        <li>Summit County, Colorado</li>
+                        <li>Tiger, Colorado</li>
+                        <li>Wagons</li>
+                    </ul>
+
+                    <p>If a number of items appear, Narrow Your Results to Photo (left-hand column)</p>
+
+                    <h3>To Find Summit County or Colorado newspapers</h3>
+
+                    <p>Go to www.coloradohistoricalnewspapers.org.</p>
+
+                    
+                    <h3>To Find the Initial Transfer of Land Title From Federal Lands to Individuals (Homesteaders, Patentees, etc) go to</h3>
+                     
+                    <p>www.glorecords.blm.gov</p>
+                         <ul>
+                             <li>If you have a potential name, enter it under "Basic" and it will pull up all properties transferred to that name.</li>
+                             <li>If you do not know of any names, but have a location, then go to "Standard" and enter the data under "Land Description"
+                                <ul>                                
+                                   <li>County - summit</li>
+                                   <li>Township NR - enter the number (all those in Summit County will be any of 2 through 8 and all will South, "S").</li>
+                                   <li>Range NR - enter the number (75 through 81 and all will be West, "W").</li>
+                                   <li>Meridian - 6th PM.</li>
+                                   <li>Section NR - All of the above data will come from either a legal description or Section Map.  Most specific Township/Range quadrants are divided into 36 sections of a square mile each or 640 acres.</li>
+                                   <li>"Search" will bring up all those transfers in that section organized by name.  Note the date and then click on "Legal Land Description" which will give you the legal description of all those parcels transferred to that name.  In "Remarks" they clarify those parcels (Lot numbers many times were rather arbitrarily assigned).  NE1/4SW1/4 read "the northeast quarter of the southwest quarter" of that section.</li>
+                               </ul>
+                            </li>
+                        </ul>
+                     
+                    <p>If help is needed with this process, it's best to email your data and questions to <a href="mailto:archives@summithistorical.org">archives@summithistorical.org</a>.</p>
+
+                     
+
+                    <h3>For research involving geology (like mines and mining) go to</h3>
+
+                    <p>The U. S. Geological Survey (USGS) http://library.usgs.gov They have two pertinent websites:</p>
+
+
+                    <ul>
+                        <li>http://www.cr.usgs.gov/fieldrecords is the archive of materials generated by scientists during their project work.  These can be searched by keywords of "summit county, colorado" or specific mines and are very detailed and technical.  The reports and maps are located at the Federal Center in Lakewood, CO, at the USGS Library and you can make copies for free while there.</li>
+                        <li>http://libraryphoto.cr.usgs.gov which gives access to the more than 37,000 online of the over 500,000 photos taken during geological studies starting in 1868 and most have never been published.  These are of high quality and can be downloaded directly.  As above, research is done through keywords by county or specific mine.</li>
+                    </ul>
+
+                    <p>The Arthur Lakes Library at the Colorado School of Mines in Golden has many maps of Summit County mining areas which can be searched at http://library.mines.edu (or Ask A Librarian at libref@mines.edu) and can be copied (11" x 17") on site, but with their library card ($45) all maps can be checked out and taken to local print shops for copying.  Again, keyword searches by county or specific mine can be done.  Their photo collection is in the process of being cataloged.</p>
+                     
+
+                    <h3>For records of water rights like irrigation/mining ditches and wells go to</h3>
+
+                    <p>www.water.state.co.us</p>
+
+                    <ul>
+                         <li>Go to "Quick LInks" (upper right) and click on "CDSS"</li>
+                         <li>"View Data" (upper left), then "Water Rights", and limit data under "Entire State" down to "Div 5 Colorado" and "36 - Blue River Basin"</li>
+                         <li>Your search can be done either by name of feature or legal location using Section, Township/Range (this is a real help).  The "Help" section is very useful.  Once the list is provided, click on the document icon at the beginning of the line and you will view the actual record, which may be long since it is a legal document and combined with many other claims in that court.</li>
+                     </ul>
+                     
+
+                    <h3>Contact The Archivists</h3>
+
+                    <p>The archivists can be contacted at <a href="mailto:archives@summithistorical.org">archives@summithistorical.org</a>. Or you can leave phone messages for them at 970-468-2207. You can send regular mail for them to P.O.Box 143, Dillon, CO. 80435</p>
+
+                    <h3>Visit the Archives</h3>
+
+                    <p>The Archives are open by appointment only. They are in the annex of the Dillon Schoolhouse Museum, 403 La Bonte Street, Dillon, Colorado. If you wish to visit them, please make an appointment using the above contact information.</p>
+
+                    <h3>Services Available</h3>
+
+                    <p><strong>Copying:</strong></p>
+
+                    <p>The Society does have a good scanner and printer and can provide copy service on a limited basis. There is a fee for this service. Larger and high quality reproductions are available locally.</p>
+
+                    <p><strong>Loans:</strong></p>
+
+                    <p>The Society has a number of artifacts and exibit photos available for loan to community organizations for special exhibits and displays. If you wish to borrow items, contact one of the archivists. A secure exhibit area is required as well as a notation that the loan is from the Summit Historical Society. Loans are usually for one year.</p>
+
+                    <p><strong>Research:</strong></p>
+
+                    <p>The Society receives numerous inquires requiring research. The volunteer archivists are willing to do limited research for each inquiry. For more extensive research, a donation to the Society is required.</p>
+
+                    <p>If you have a reference or link you would like us to add to our website, please email us at <a href="mailto:archives@summithistorical.org">archives@summithistorical.org</a>.</p>
+                </div>
+            </div>  
+        </div>
+
+        <!-- ################ PAGE CONTENT ################  -->
+
+        <div class="space-70"></div>
 
         <!-- load footer -->
         <?php include("assets/templates/footer.php");?>
