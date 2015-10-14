951ca38642c10688affc9fea3f770de60f86e51b
Graham Hukill <ghukill@gmail.com>
2015-10-13 20:37:29 -0400

    pre pull request

--- donors.php
+++ donors.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-        <title>Summit Historical Society</title>
+        <title>Summit Historical Society - Donors</title>
         <meta name="description" content="HTML5 & CSS3">
         <meta name="keywords" content="HTML5, CSS3, Theme,bootstrap3,clean design"> 
 
