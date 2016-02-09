c9ea9f39aa6e5b257eecebad93e2f99591ffeaef
Graham Hukill <ghukill@gmail.com>
2016-02-08 19:05:38 -0500

    added 'Ways to Give' section

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -43,12 +43,13 @@
                             <li><a href="newsletter.php">Newsletter</a></li>  
                         </ul>
                     </li>
+                    <li><a href="waystogive.php">Ways to Give</a></li>  
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle js-activated" data-toggle="dropdown">Get Involved</a>
                         <ul class="dropdown-menu">
                             <li><a href="membership.php">Membership</a></li>                              
                             <li><a href="volunteer.php">Volunteer</a></li>
-                            <!-- <li><a href="donors.php">Donors</a></li> -->
+                            <li><a href="waystogive.php">Ways to Give</a></li>
                         </ul>
                     </li>
                     <li><a href="contact.php">Contact</a></li>
---
+++ waystogive.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+        <title>Summit Historical Society</title>
+        <meta name="description" content="HTML5 & CSS3">
+        <meta name="keywords" content="HTML5, CSS3, Theme,bootstrap3,clean design"> 
+
+        <!--bootstrap3 css-->
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!--ion icon fonts css-->
+        <link href="assets/icons/css/ionicons.css" rel="stylesheet">
+        <!--custom css-->
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <!--google  font family-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400italic' rel='stylesheet' type='text/css'>
+        <!--flex slider css-->
+        <link href="assets/css/flexslider.css" rel="stylesheet">
+        <!--animated  css-->
+        <link href="assets/css/animate.css" rel="stylesheet">
+        
+        <!-- Local CSS Overrides -->
+        <link href="assets/css/local.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+
+        <!-- load header -->
+        <?php include("assets/templates/header.php");?>
+
+        <div class="space-70"></div>
+
+        <!-- ################ PAGE CONTENT ################  -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h1>Ways to Give</h1>
+
+                    <div class="space-30"></div>
+
+					<h3>Individual Giving</h3>
+
+					<p>Members and friends can support the Society with their financial gifts.  Many give above and 
+
+					beyond their dues, becoming Guardians of our Historical Heritage.  Some give gifts in honor of 
+
+					others; or in memory of friends and family.  You can honor someone with a gift at a birthday, 
+
+					anniversary, or holiday, such as Father’s Day, Mother’s Day, or Christmas.</p>
+
+					<div class="space-30"></div>
+
+					
+					<h3>Stocks, Bonds and Planned Giving</h3>
+
+					<p>We accept stocks and bonds in our Charles Schwab investment account.  Gifts benefit our various 
+
+					programs/projects:  cataloging in the Rutledge Archive, the living classroom in the schoolroom, the 
+
+					planned high-altitude ranching and agriculture exhibits in the Rice barn; or general operating 
+
+					expenses.  It is possible to designate a program or project supported by your gift.  
+
+					We also accept bequests, charitable remainder trusts and other planned giving options of your 
+
+					choice.</p>
+
+					<div class="space-30"></div>
+					
+					<h3>Local Community Giving</h3>					
+
+					<div style="padding:20px 0px 0px 20px;">
+
+						<h4>Colorado Gives Day</h4>
+
+						<p>Each December, people have the opportunity to give to the non-profit organizations of their choice 
+
+						through Colorado Gives Day.  Watch for details each fall beginning in November.</p>
+
+						<div class="space-20"></div>
+
+						<h4>City Market Rewards Program & Amazon Smile</h4>
+						<p>You can also enroll in the City Market Rewards Program and Amazon Smile.  Simply follow the 
+
+						instructions below to designate the SHS as your chosen charity, and the Society will receive a 
+
+						percentage of your everyday purchases.  This is an amazing way to support the SHS without 
+
+						breaking the bank.</p>
+
+						<p>To enroll in the <strong>City Market Community Rewards program</strong>:
+							<ol>
+								<li>Visit <a href="https://www.citymarket.com/topic/city-market-community-rewards">City Market Rewards</a>; choose City Market Rewards Program.</li>
+								<li>Create an online account with CMR by clicking  Enroll Now.</li>
+								<li>Once the account is created, you will be logged into the program.</li>
+								<li>Type in the charity of choice—Summit Historical Society.</li>
+								<li>Click on the circle to assign your card to SHS and you’re done.</li>
+							</ol>
+						</p>
+
+						<p>To enroll in <strong>Amazon Smile</strong>:
+							<ol>
+								<li>Go to <a href="http://smile.amazon.com">smile.amazon.com</a>.</li>
+
+								<li>Type "Summit Historical Society" in the bottom right-hand corner where the box asks for a charity.</li>
+
+								<li>Then click SELECT. You should get a response that your purchases will now support SHS.</li>
+
+								<li>Be sure to visit Smile.Amazon.com every time you purchase from Amazon – ordering through the regular Amazon.com will not benefit the SHS.</li>
+							</ol>
+						</p>
+
+					</div>
+					 
+					<div class="space-20"></div>
+
+					<h3>Car Donations through Vehicles of Charity</h3>
+
+					<p>SHS is a member of the Vehicles for Charity program.  It is a very simple process to donate.  Contact 
+
+					the SHS office for the phone number of the Vehicles for Charity program.  The Society gets 80 
+
+					percent of the sale price of the vehicle; Vehicles for Charity keeps 20 percent.  Cars should not be 
+
+					more than five years old at the time of donation.</p>
+
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- ################ PAGE CONTENT ################  -->
+
+        <div class="space-70"></div>
+
+        <!-- load footer -->
+        <?php include("assets/templates/footer.php");?>
+
+        <!--scripts-->
+        <script src="assets/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="assets/js/modernizr.custom.97074.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.easing.1.3.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.sticky.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.flexslider-min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.mixitup.min.js" type="text/javascript"></script>
+        <script src="assets/js/parallax.min.js" type="text/javascript"></script>
+        <script src="assets/js/wow.min.js" type="text/javascript"></script>
+        <script src="assets/js/app.js" type="text/javascript"></script>
+    </body>
+</html>
